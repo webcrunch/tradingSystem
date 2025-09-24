38794e2cc325f027d23868fcabe5e74a232fb011
jarl <jarllindquist@gmail.com>
2025-09-24 11:43:52 +0200

    Create folder for classes and a new class for functions for utils

---
+++ Classes/Utils.cs
@@ -0,0 +1,11 @@
+// namespace TradingSystem;
+
+
+
+// public static void PrintMain()
+// {
+//     Console.WriteLine("-----Trading System -------");
+//     Console.WriteLine("1. Create account");
+//     Console.WriteLine("2. Login");
+//     Console.WriteLine("3. Exit");
+// }
