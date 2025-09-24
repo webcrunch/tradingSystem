1fff53a77a3c09076405a8316a7f481bb1741481
jarl <jarllindquist@gmail.com>
2025-09-24 11:59:51 +0200

    Create a new class for users

---
+++ trading_system/Classes/User.cs
@@ -0,0 +1,13 @@
+namespace TradingSystem;
+
+public class user
+{
+    public string Username
+    string _password
+
+    public user(string username, string password)
+    {
+        Username = username;
+        _password = password;
+    }
+}
